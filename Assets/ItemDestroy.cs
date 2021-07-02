e0d89b09fa7f52b01a17f36df211f36fae7ee88c
phosphorousJ <86190512+phosphorousJ@users.noreply.github.com>
2021-07-02 23:38:41 +0900

    Initial commit

---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour
+{
+    //Unityちゃんのゲームオブジェクト
+    private GameObject unitychan;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //オブジェクトのz座標がunityちゃんのz座標より小さい時、オブジェクトを破棄
+        if (unitychan.transform.position.z > this.gameObject.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
